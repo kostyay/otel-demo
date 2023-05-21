a5dea6dd6b492ddd33274287298d09d065609320
kostyay <kostya.y@gmail.com>
2023-05-21 23:34:43 +0300

    more

--- controller/internal/math/math.go
+++ controller/internal/math/math.go
@@ -5,10 +5,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/kostyay/otel-demo/common/otel"
+	"github.com/kostyay/otel-demo/common/log"
+	otelcommon "github.com/kostyay/otel-demo/common/otel"
 	otelpubsub "github.com/kostyay/otel-demo/common/otel/pubsub"
 	pb "github.com/kostyay/otel-demo/controller/api/calculator/v1"
 	"github.com/kostyay/otel-demo/controller/internal/config"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type Storage interface {
@@ -48,7 +51,46 @@ func New(ctx context.Context, cfg *config.Options, storage Storage) (*handler, e
 		return nil, fmt.Errorf("unable to check response subscription existence: %w", err)
 	}
 
+	err = result.responseSub.Receive(ctx, otelpubsub.WrapPubSubHandlerWithTelemetry(otelcommon.Tracer(), cfg.MathResultSubscription, result.handleMathResult))
+	if err != nil {
+		requestClient.Close()
+		return nil, fmt.Errorf("unable to start receiving messages: %w", err)
+	}
+
 	return result, nil
+}
+
+func (h *handler) handleMathResult(ctx context.Context, msg *pubsub.Message) {
+	var calculation pb.Calculation
+
+	var err error
+	span := trace.SpanFromContext(ctx)
+
+	defer func() {
+		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+		}
+		span.End()
+	}()
+
+	logger := log.WithContext(ctx)
+
+	err = json.Unmarshal(msg.Data, &calculation)
+	if err != nil {
+		logger.WithError(err).Error("unable to unmarshal calculation")
+		return
+	}
+
+	err = h.storage.UpdateResult(ctx, uint(calculation.Id), calculation.Result)
+	if err != nil {
+		logger.WithError(err).Error("unable to update result")
+		return
+	}
+
+	span.AddEvent("result updated")
+
+	msg.Ack()
 }
 
 func (h *handler) Calculate(ctx context.Context, calculation *pb.Calculation) error {
@@ -61,7 +103,7 @@ func (h *handler) Calculate(ctx context.Context, calculation *pb.Calculation) er
 		Data: expression,
 	}
 	// Create a new span
-	ctx, span := otelpubsub.BeforePublishMessage(ctx, otel.Tracer(), h.requestTopic.String(), msg)
+	ctx, span := otelpubsub.BeforePublishMessage(ctx, otelcommon.Tracer(), h.requestTopic.String(), msg)
 	defer span.End()
 
 	result, err := h.requestTopic.Publish(ctx, msg).Get(ctx)
